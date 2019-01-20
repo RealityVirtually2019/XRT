bee0bb9ede6e5a356016fec87de9e232c85da5fb
Travis Ho <travisho@Traviss-MacBook-Pro.local>
2019-01-20 13:21:21 -0500

    add vuforia version

---
+++ mobile/Unity + Vuforia app/XRT App/Assets/animate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animation myAnimationComponent;
+
+    void Start()
+    {
+        myAnimationComponent = GetComponent<Animation>();
+        myAnimationComponent.Play("Take001");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
